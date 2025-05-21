75b4b5d9710eaa094cde3169c2f205f575b7459b
GD3sign <gigisgasparaitis@gmail.com>
2025-05-22 00:22:07 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# AutoGvidas
+
+Demo automobilių skelbimų forma su HTML, CSS ir JavaScript.
---
+++ main.js
@@ -0,0 +1,5 @@
+
+document.querySelector("form").addEventListener("submit", function(e) {
+    e.preventDefault();
+    alert("Skelbimas įkeltas (tik demo)");
+});
